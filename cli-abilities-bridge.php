3400503fee931c35df3771bb917f15a28924ef5f
David Stone <david@nnucomputerwhiz.com>
2026-08-19 01:45:18 -0600

    chore: mark tested up to WordPress 7.1

--- cli-abilities-bridge.php
+++ cli-abilities-bridge.php
@@ -10,7 +10,7 @@
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: cli-abilities-bridge
  * Requires at least: 6.8
- * Tested up to:      7.0
+ * Tested up to:      7.1
  * Requires PHP:      7.4
  */
 
